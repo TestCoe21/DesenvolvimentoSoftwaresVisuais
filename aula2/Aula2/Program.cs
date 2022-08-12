08ade168ef1f7225f7005a9bbb06aaeb780e0ccc
Rafa Pavelski <rafapavelski@gmail.com>
2022-08-11 22:11:26 -0300

    att

---
+++ aula2/Aula2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Informe uma largura:");
+                double largura = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Informe uma altura:");
+                double altura = Convert.ToDouble(Console.ReadLine());
+                double area = CalculaArea(largura, altura);
+                Console.WriteLine("A área é " + area);
+            }
+            catch(Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+        }
+
+    static double CalculaArea(double largura, double altura)
+    {
+        return largura * altura;
+    }
+
+    }
+}
